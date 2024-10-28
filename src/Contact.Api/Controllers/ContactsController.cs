20df1eede687dcf0d471e4c084596a6835f6dd47
Raphael Rogenski <raphaelrogenski@gmail.com>
2024-10-27 22:19:17 -0300

    ContactController: Just creating the skeleton with main operations to be executed (and for sure, implemented).

---
+++ src/Contact.Api/Controllers/ContactsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Contacts.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{areaCode}")]
+        public IActionResult GetByAreaCode(string areaCode)
+        {
+            return Ok();
+        }
+
+        [HttpPost()]
+        public IActionResult Create()
+        {
+            return Ok();
+        }
+
+        [HttpPut()]
+        public IActionResult Update()
+        {
+            return Ok();
+        }
+
+        [HttpDelete()]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+    }
+}
