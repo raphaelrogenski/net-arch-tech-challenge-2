a181d658c30be8a1ab3838345a14c87154520a83
Raphael Rogenski <raphaelrogenski@gmail.com>
2024-10-29 09:24:53 -0300

    Adding initial tests for application. Achieving 26% of Line Coverage and 12% of Branch Coverage.

---
+++ src/Contact.Test/Tests/ApplicationTest.cs
@@ -0,0 +1,18 @@
+﻿namespace Contacts.Test.Tests
+{
+    public class ApplicationTest
+    {
+        [Fact]
+        public void Run_ShouldRunFine()
+        {
+            // Arrange
+            var application = new Api.Application();
+
+            // Act
+            application.Run([]);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
