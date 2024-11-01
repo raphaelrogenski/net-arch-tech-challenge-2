2dd9bd19208ef85679cdd8e662280990aefb50a8
Raphael Rogenski <raphaelrogenski@gmail.com>
2024-11-01 09:43:50 -0300

    Adding SonarScanner publisher for Build and Tests.

--- src/Contact.Test/test.bat
+++
@@ -1,3 +0,0 @@
-rmdir /S /Q "TestResults/"
-dotnet test --collect:"XPlat Code Coverage" -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.ExcludeByFile="**/Migrations/*.cs"
-reportgenerator -reports:"./TestResults/**/coverage.cobertura.xml" -targetdir:"./TestResults/CoverageReport" -reporttypes:Html
